6029a55593101fb3b08acb9eeb5a676a1ff9a206
Augusto Monge <augustomonge@MacBook-Pro-de-Augusto.local>
2026-04-09 14:49:50 -0600

    update main for now

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
-from database import engine, Base
-from routers import items
+#from database import engine, Base
+#from routers import items
 
-Base.metadata.create_all(bind=engine)
+#Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-app.include_router(items.router)
+#app.include_router(items.router)
 
 @app.get("/")
 def root():
